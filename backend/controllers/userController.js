6790bac12b1683afd77aa0195ac9e2955bf4141e
keshav <keshav@example.com>
2026-07-18 08:55:27 +0530

    update login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,47 @@
+const getDbPool = require("../db");
+const bcrypt = require("bcrypt");
+
+
 const login = async (req, res) => {
-    console.log("Login API called");
+     try {
 
-    const { email, password } = req.body;
+        console.log("Login API called");
+        const { email, password } = req.body;
+        const [rows] = await pool.execute("SELECT * FROM jc_web_pros_users WHERE email = ?",[email]);
+        if (rows.length === 0) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Email or Password",
+            });
+            }
+            const user = rows[0];
+           const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Invalid Email or Password",
+                });
+                } 
+                return res.status(200).json({
+                success: true,
+                message: "Login Successful",
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                },
+                });
+            }
+            catch (error) {
+                console.error(error);
 
-    res.json({
-        success: true,
-        message: "Login Successful",
-        email,
-        password
-    });
-};
+                return res.status(500).json({
+                success: false,
+                message: "Internal Server Error",
+                });
+            }
+        };
 
-const register = async (req, res) => {
-    res.json({
-        success: true,
-        message: "Register Successful"
-    });
-};
-
-module.exports = {
-    login,
-    register
-};
+    module.exports = {
+        login,
+    };
